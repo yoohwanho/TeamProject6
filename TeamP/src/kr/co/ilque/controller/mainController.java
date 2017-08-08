acfbb2481e1d3298a4d24fe3d7002acb0d153e08
yoohwanho <youhoanho@naver.com>
2017-08-08 14:41:38 +0900

    네이게이터,메인로고 클릭시 해당페이지로 이동할 수 있게만 컨트롤러,ctx 생성 및 web.xml서블릿,서블릿맵핑 추가 및 각 뷰페이지 코드 수정

---
+++ TeamP/src/kr/co/ilque/controller/mainController.java
@@ -0,0 +1,30 @@
+package kr.co.ilque.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class mainController {
+
+	@RequestMapping("/main")
+	public String main() {
+		return "main";
+	}
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+	@RequestMapping("/join")
+	public String join() {
+		return "join";
+	}
+	@RequestMapping("/writeForm")
+	public String writeForm() {
+		return "writeForm";
+	}
+	@RequestMapping("/board")
+	public String board() {
+		return "board";
+	}
+	
+}
