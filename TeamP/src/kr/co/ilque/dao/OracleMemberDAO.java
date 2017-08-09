60cfb9d1148188d574fa80e165ae1d44979561f6
user <user@user-PC>
2017-08-09 12:33:56 +0900

    save

--- TeamP/src/kr/co/ilque/dao/OracleMemberDAO.java
+++ TeamP/src/kr/co/ilque/dao/OracleMemberDAO.java
@@ -1,8 +1,18 @@
 package kr.co.ilque.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
+
+import kr.co.ilque.dto.MemberDto;
 
 public class OracleMemberDAO {
 	SqlSession ss;
+	
+	public List<MemberDto> selectAll(){
+		
+		return ss.selectList("kr.)
+		
+	}
 	
 }
