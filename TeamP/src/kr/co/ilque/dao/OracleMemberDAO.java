335fcdbb5997f2472d00524e5c1e41fcf6567247
Anjooyeon <happyi998@hanmail.net>
2017-08-09 14:54:16 +0900

    주연 joinController 작성

--- TeamP/src/kr/co/ilque/dao/OracleMemberDAO.java
+++ TeamP/src/kr/co/ilque/dao/OracleMemberDAO.java
@@ -2,7 +2,17 @@ package kr.co.ilque.dao;
 
 import org.apache.ibatis.session.SqlSession;
 
+import kr.co.ilque.dto.MemberDto;
+
 public class OracleMemberDAO {
 	SqlSession ss;
 	
+	public void setSs(SqlSession ss) {
+		this.ss = ss;
+	}
+
+
+	public void insertOne(MemberDto dto) {
+		ss.insert("kr.co.ilque.member.insertOne",dto);
+	}
 }
--- TeamP/src/kr/co/ilque/service/UserService.java
+++ TeamP/src/kr/co/ilque/service/UserService.java
@@ -1,5 +1,15 @@
 package kr.co.ilque.service;
 
+import kr.co.ilque.dao.OracleMemberDAO;
+import kr.co.ilque.dto.MemberDto;
+
 public class UserService implements Service{
+	//회원 가입, 수정
+	
+	OracleMemberDAO dao;
+	
+	public void insertOne(MemberDto dto) {
+		dao.insertOne(dto);
+	}
 
 }
