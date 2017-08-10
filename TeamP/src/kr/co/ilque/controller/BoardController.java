9f3987d82a1de5aa3031165a434e7b1b40baf17b
user <user@user-PC>
2017-08-10 13:19:53 +0900

    writeform 작성중

--- TeamP/src/kr/co/ilque/controller/BoardController.java
+++ TeamP/src/kr/co/ilque/controller/BoardController.java
@@ -1,6 +1,7 @@
 package kr.co.ilque.controller;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.co.ilque.dto.BoardDto;
+import kr.co.ilque.dto.DetailViewDto;
+import kr.co.ilque.dto.MemberDto;
 import kr.co.ilque.service.BoardService;
 import kr.co.ilque.service.CommentsService;
+import kr.co.ilque.service.LoginService;
 
 @Controller
 public class BoardController {
@@ -17,6 +22,17 @@ public class BoardController {
 	BoardService bs;
 	@Resource(name="commentsService")
 	CommentsService cs;
+/*	@Resource(name="loginService")
+	LoginService ls;
+	*/
+
+	public void setBs(BoardService bs) {
+		this.bs = bs;
+	}
+
+	public void setCs(CommentsService cs) {
+		this.cs = cs;
+	}
 
 	// 거래목록에서 글 누르면 상세페이지로 넘어감
 	// bno를 통하여 db에서 글의 상세정보 가져오기
@@ -35,7 +51,52 @@ public class BoardController {
 
 	// 작성 버튼 누르면 글 작성 정보를 가지고 글 상세페이지로 넘어감
 	@RequestMapping(value = "/writeOk", method = RequestMethod.POST)
-	public String writeOk() {
+	public String writeOk(
+			@RequestParam("category")String category,
+			@RequestParam("title")String title,
+			@RequestParam("contents")String contents,
+			@RequestParam("reward")int reward,
+			@RequestParam("phone")String phone,
+			@RequestParam("time")String time,
+			@RequestParam("sample6_postcode")String postCode,
+			@RequestParam("sample6_address")String address,
+			@RequestParam("sample6_address2")String address2,
+			HttpSession ss) {
+		
+		System.out.println("writeOk 진입");
+		BoardDto bdto= new BoardDto();
+		String writer = (String) ss.getAttribute("id");//writer에 세션에서 아이디 받아와서 저장
+		String loc = address+" "+address2+" (우)"+postCode;
+
+		
+		
+		System.out.println("writer:"+writer);
+		System.out.println("category:"+category);
+		System.out.println("title:"+title);
+		System.out.println("contents:"+contents);
+		System.out.println("reward:"+reward);
+		System.out.println("phone:"+phone);
+		System.out.println("time:"+time);
+		System.out.println("postCode:"+postCode);
+		System.out.println("address:"+address);
+		System.out.println("address2:"+address2);
+		
+		bdto.setCategory(category);
+		bdto.setTitle(title);
+		bdto.setContents(contents);
+		bdto.setWriter(writer);
+		bdto.setLoc(loc);
+		bdto.setTime(time);
+		bdto.setPhone(phone);
+		bdto.setReward(reward);
+		bdto.setApplicant("");
+		
+		
+		
+		System.out.println();
+		
+		
+		
 		return null;
 		// "detail?bno="+bno;
 	}
